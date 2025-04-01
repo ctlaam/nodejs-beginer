9ae552a4632481cdf62ed0da310a3fdbcc90d9eb
your_name <your_email@abc.example>
2025-04-01 16:58:39 +0700

    hi

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -3,3 +3,39 @@
  * YouTube: https://youtube.com/@trungquandev
  * "A bit of fragrance clings to the hand that gives flowers!"
  */
+
+
+const MONGODB_URI = 'mongodb+srv://liamdev:Lam140920@cluster0.kvfee.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+
+const DATABASE_NAME = 'liamdev'
+
+import { MongoClient, ServerApiVersion } from 'mongodb'
+
+let trelloDatanaseInstance = null
+const mongoClientInstance = new MongoClient(MONGODB_URI, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+})
+
+export const CONNECT_DB = async () => {
+    try {
+        if (!trelloDatanaseInstance) {
+            await mongoClientInstance.connect()
+            trelloDatanaseInstance = mongoClientInstance.db(DATABASE_NAME)
+        }
+        return trelloDatanaseInstance
+    } catch (error) {
+        console.log('MongoDB connection error: ', error)
+    }
+}
+
+export const GET_DB = () => {
+    if (!trelloDatanaseInstance) {
+        throw new Error('Database not connected. Please call CONNECT_DB first.')
+    }
+    return trelloDatanaseInstance
+
+}
--- src/server.js
+++ src/server.js
@@ -5,28 +5,31 @@
  */
 
 import express from 'express'
-import { mapOrder } from '~/utils/sorts.js'
-
+import { CONNECT_DB, GET_DB } from './config/mongodb'
 const app = express()
+const START_SERVER = () => {
+  const hostname = 'localhost'
+  const port = 8017
 
-const hostname = 'localhost'
-const port = 8017
+  app.get('/', async (req, res) => {
+    console.log(await GET_DB().listCollections().toArray())
+    res.end('<h1>Hello World!</h1><hr>')
+  })
 
-app.get('/', (req, res) => {
-  // Test Absolute import mapOrder
-  console.log(mapOrder(
-    [ { id: 'id-1', name: 'One' },
-      { id: 'id-2', name: 'Two' },
-      { id: 'id-3', name: 'Three' },
-      { id: 'id-4', name: 'Four' },
-      { id: 'id-5', name: 'Five' } ],
-    ['id-5', 'id-4', 'id-2', 'id-3', 'id-1'],
-    'id'
-  ))
-  res.end('<h1>Hello World!</h1><hr>')
-})
+  app.listen(port, hostname, () => {
+    // eslint-disable-next-line no-console
+    console.log(`Hello TI am running at ${hostname}:${port}/`)
+  })
+}
+(async () => {
+  try {
+    console.log('1. Connecting to MongoDB...')
+    await CONNECT_DB()
+    console.log('2. Connected to MongoDB successfully!')
+    START_SERVER()
+  } catch (error) {
+    console.log('MongoDB connection error: ', error)
+    process.exit(0)
+  }
+})()
 
-app.listen(port, hostname, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Hello Trung Quan Dev, I am running at ${ hostname }:${ port }/`)
-})
